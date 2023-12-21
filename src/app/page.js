469a6bf145cfe5fd2aacc1e4c341aeda928ce028
Budi Ariyansa <budi.ariyansa@sat.co.id>
2023-12-21 16:00:42 +0700

    update again

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,40 @@
 import Image from 'next/image'
-import Table from '../../components/Table'
 
-export default function Home() {
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+    const { rows } = await sql`SELECT * FROM list_kdrama`
     return (
         <main>
             <div className='container mx-auto'>
                 <h2 className='text-center text-2xl py-5 font-bold'>K-Drama List Budi</h2>
 
-                <div className='flex justify-center p-2'>
-                    <Table />
+                <div className='flex justify-center'>
+                    <table className='m-3 border-2 border-white'>
+                        <thead className="text-left bg-cyan-800">
+                            <tr>
+                                <th className='border-2 border-white p-2'>No.</th>
+                                <th className='border-2 border-white p-2'>Drama Name</th>
+                                <th className='border-2 border-white p-2'>Relase Year</th>
+                                <th className='border-2 border-white p-2'>Rating</th>
+                                <th className='border-2 border-white p-2'>Watching On</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            rows.map((row) => (
+                                <tr key={row.kdrama_id} >
+                                    <td className='border-2 border-white p-2'>{row.kdrama_id}</td>
+                                    <td className='border-2 border-white p-2'>{row.kdrama_name}</td>
+                                    <td className='border-2 border-white p-2'>{row.kdrama_release_date}</td>
+                                    <td className='border-2 border-white p-2'>{row.kdrama_rating}</td>  
+                                    <td className='border-2 border-white p-2'>{row.kdrama_watchin_on}</td>  
+                                </tr>
+                            ))
+                        }
+                        </tbody>
+                        
+                    </table>
                 </div>
             </div>
         </main>
