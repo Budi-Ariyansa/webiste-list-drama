1a7be11f6ce66719b0028f3dd1e965a13392d00f
Budi Ariyansa <budi.ariyansa@sat.co.id>
2023-12-21 15:39:16 +0700

    update data dengan database

--- components/Table.js
+++ components/Table.js
@@ -1,5 +1,7 @@
-export default function Table() {
-    const data = ['1.', 'Bussines Proposal', '2020', '10/10']
+import { sql } from "@vercel/postgres";
+
+export default async function Table() {
+    const { rows } = await sql`SELECT * FROM list_kdrama`
     return (
         <table className='table-auto border-2 border-white'>
             <thead>
@@ -8,15 +10,23 @@ export default function Table() {
                     <th className='border-2 border-white p-2'>Drama Name</th>
                     <th className='border-2 border-white p-2'>Relase Year</th>
                     <th className='border-2 border-white p-2'>Rating</th>
+                    <th className='border-2 border-white p-2'>Watching On</th>
                 </tr>
             </thead>
-            <tr>
-                {
-                    data && data.map((item, index) => (
-                        <td key={index} className='border-2 border-white p-2'>{item}</td>
-                    ))
-                }
-            </tr>
+            <tbody>
+            {
+                rows.map((row) => (
+                    <tr key={row.kdrama_id} >
+                        <td className='border-2 border-white p-2'>{row.kdrama_id}</td>
+                        <td className='border-2 border-white p-2'>{row.kdrama_name}</td>
+                        <td className='border-2 border-white p-2'>{row.kdrama_release_date}</td>
+                        <td className='border-2 border-white p-2'>{row.kdrama_rating}</td>  
+                        <td className='border-2 border-white p-2'>{row.kdrama_watchin_on}</td>  
+                    </tr>
+                ))
+            }
+            </tbody>
+            
         </table>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Table from '../../components/Table'
 
 export default function Home() {
     return (
-        <div className='container mx-auto'>
-            <h2 className='text-center text-2xl py-5 font-bold'>K-Drama List Budi</h2>
+        <main>
+            <div className='container mx-auto'>
+                <h2 className='text-center text-2xl py-5 font-bold'>K-Drama List Budi</h2>
 
-            <div className='flex justify-center p-2'>
-                <Table />    
+                <div className='flex justify-center p-2'>
+                    <Table />
+                </div>
             </div>
-        </div>
+        </main>
     )
 }
