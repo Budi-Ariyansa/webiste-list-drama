08dcf278487f46a9de98d475df2dca9050fd55d9
Budi Ariyansa <budi.ariyansa@sat.co.id>
2023-12-26 14:16:11 +0700

    update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from 'next/font/google'
+import { Source_Sans_3 } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+const SS3 = Source_Sans_3({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'List K-Drama Budi',
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 	<html lang="en">
-		<body className={inter.className}>{children}</body>
+		<body className={SS3.className}>{children}</body>
 	</html>
 	)
 }
--- src/app/page.js
+++ src/app/page.js
@@ -9,49 +9,52 @@ export default async function Home() {
         from list_kdrama
     `
     return (
-        <main>
-            <div className='container mx-auto'>
-                <h2 className='text-center py-5 font-bold text-xl md:text-3xl'>My Favorite K-Drama</h2>
-
-                <div className='flex justify-center'>
-                    <div className='flex flex-col'>
-                        {
-                            rows.map((drama) => (
-                                <div className='card rounded-2xl shadow-xl flex flex-col m-3 p-3 md:flex-row' key={drama.kdrama_id}>
-                                    <div className='shrink'>
-                                        <Image
-                                            src={drama.kdrama_image_url}
-                                            alt={drama.kdrama_name}
-                                            width={250}
-                                            height={300}
-                                            className='w-auto h-auto rounded-t-lg'
-                                        />
+        <>
+            <div className='header bg-slate-500'>
+                <h2 className='text-white text-center py-5 font-bold text-xl md:text-3xl'>My Favorite K-Drama</h2>
+            </div>
+            <main>
+                <div className='container mx-auto'>
+                    <div className='flex justify-center'>
+                        <div className='flex flex-col'>
+                            {
+                                rows.map((drama) => (
+                                    <div className='card rounded-2xl shadow-xl flex flex-col m-3 p-3 md:flex-row' key={drama.kdrama_id}>
+                                        <div className='shrink'>
+                                            <Image
+                                                src={drama.kdrama_image_url}
+                                                alt={drama.kdrama_name}
+                                                width={250}
+                                                height={300}
+                                                className='w-auto h-auto rounded-t-lg'
+                                            />
+                                        </div>
+                                        <div className='flex-col pl-0 md:pl-3'>
+                                            <p className='text-xl md:w-[350px] w-[320px] md:text-2xl font-bold mb-3 mt-2'>{drama.kdrama_name}</p>
+                                            <div className='flex text-[13px] md:text-base'>
+                                                <p className='font-semibold'>Total Episode :</p>
+                                                <p className='pl-1'>{drama.kdrama_total_episode} episode</p>
+                                            </div>
+                                            <div className='flex text-[13px] md:text-base'>
+                                                <p className='font-semibold'>Publish Date :</p>
+                                                <p className='pl-1'>{drama.kdrama_publish_date}</p>
+                                            </div>
+                                            <div className='flex text-[13px] md:text-base'>
+                                                <p className='font-semibold'>My Rating :</p>
+                                                <p className='pl-1'>{drama.kdrama_rating}/10</p>
+                                            </div>
+                                            <div className='flex text-[13px] md:text-base'>
+                                                <p className='font-semibold'>Watch :</p>
+                                                <p className='pl-1 md:w-[350px] w-[250px]'>{drama.kdrama_where_to_watch}</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className='flex-col pl-0 md:pl-3'>
-                                        <p className='text-xl md:w-[350px] w-[250px] md:text-2xl font-bold mb-3 mt-2'>{drama.kdrama_name}</p>
-                                        <div className='flex text-[12px] md:text-base'>
-                                            <p>Total Episode :</p>
-                                            <p className='pl-1'>{drama.kdrama_total_episode} episode</p>
-                                        </div>
-                                        <div className='flex text-[12px] md:text-base'>
-                                            <p>Publish Date :</p>
-                                            <p className='pl-1'>{drama.kdrama_publish_date}</p>
-                                        </div>
-                                        <div className='flex text-[12px] md:text-base'>
-                                            <p>My Rating :</p>
-                                            <p className='pl-1'>{drama.kdrama_rating}/10</p>
-                                        </div>
-                                        <div className='flex text-[12px] md:text-base'>
-                                            <p>Watch :</p>
-                                            <p className='pl-1 md:w-[350px] w-[250px]'>{drama.kdrama_where_to_watch}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        </>
     )
 }
