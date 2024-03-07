67282d3c3173f661b9fe7bb662e8fda2430709ac
Budi Ariyansa <budi.ariyansa@sat.co.id>
2024-03-07 15:05:56 +0700

    test

--- src/app/admin/submit.js
+++ src/app/admin/submit.js
@@ -18,6 +18,7 @@ export default async function handler(req, res) {
             }
         }
         
+        
         const client = createClient()
         await client.connect()
         try {
